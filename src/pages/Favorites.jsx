49997b6d804d27052efba0090110d622800d1748
Prem <premadh7@gmail.com>
2026-03-05 21:01:19 +0545

    Implement favorites display in Favorites component using MovieContext

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,6 +1,23 @@
 import "../css/Favorites.css"
+import { useMovieContext } from "../context/MovieContext"
+import MovieCard from "../component/MovieCard"
 
 function Favorites() {
+
+    const { favorites } = useMovieContext()
+
+    if(favorites) {
+        return (
+        <div className="favorites"> 
+            <h2>Your Favorites</h2>
+        <div className="movie-grid">
+            {favorites.map((movie) => (
+                <MovieCard key={movie.id} movie={movie} />
+            ))}
+        </div>
+        </div>
+        )
+    }
     return (
         <div className="favorites-empty">
             <h2>No favorites yet. </h2>
