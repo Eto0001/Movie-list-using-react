d39a9536cfc949705430f591450b76f1b50f64f6
Prem <premadh7@gmail.com>
2026-03-05 20:39:23 +0545

    Wrap application in MovieProvider for context management

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import Favorites from "./pages/Favorites"
 import Home from "./pages/Home"
 import {Routes, Route} from "react-router-dom"
 import NavBar from "./component/NavBar"
+import { MovieProvider } from "./context/MovieContext";
 
 function App() {
 
   return (
-    <div>
+    <MovieProvider>
       <NavBar />
     <main className="main-content">
       <Routes>
@@ -15,7 +16,7 @@ function App() {
         <Route path="/favorites" element={<Favorites />} />
       </Routes>
     </main>
-    </div>
+    </MovieProvider>
   );
 }
 
